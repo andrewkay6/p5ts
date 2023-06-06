763bdf7ebd7f373818e3efe87f8dca72f27da92c
Andrew (kaya6) <kaya6@mcmaster.ca>
2023-06-06 17:09:27 -0400

    Added the game logic skeleton.

--- build/build.js
+++ build/build.js
@@ -1,93 +1,107 @@
-var ColorHelper = (function () {
-    function ColorHelper() {
-    }
-    ColorHelper.getColorVector = function (c) {
-        return createVector(red(c), green(c), blue(c));
-    };
-    ColorHelper.rainbowColorBase = function () {
-        return [
-            color('red'),
-            color('orange'),
-            color('yellow'),
-            color('green'),
-            color(38, 58, 150),
-            color('indigo'),
-            color('violet')
-        ];
-    };
-    ColorHelper.getColorsArray = function (total, baseColorArray) {
-        var _this = this;
-        if (baseColorArray === void 0) { baseColorArray = null; }
-        if (baseColorArray == null) {
-            baseColorArray = ColorHelper.rainbowColorBase();
-        }
-        var rainbowColors = baseColorArray.map(function (x) { return _this.getColorVector(x); });
-        ;
-        var colours = new Array();
-        for (var i = 0; i < total; i++) {
-            var colorPosition = i / total;
-            var scaledColorPosition = colorPosition * (rainbowColors.length - 1);
-            var colorIndex = Math.floor(scaledColorPosition);
-            var colorPercentage = scaledColorPosition - colorIndex;
-            var nameColor = this.getColorByPercentage(rainbowColors[colorIndex], rainbowColors[colorIndex + 1], colorPercentage);
-            colours.push(color(nameColor.x, nameColor.y, nameColor.z));
-        }
-        return colours;
-    };
-    ColorHelper.getColorByPercentage = function (firstColor, secondColor, percentage) {
-        var firstColorCopy = firstColor.copy();
-        var secondColorCopy = secondColor.copy();
-        var deltaColor = secondColorCopy.sub(firstColorCopy);
-        var scaledDeltaColor = deltaColor.mult(percentage);
-        return firstColorCopy.add(scaledDeltaColor);
-    };
-    return ColorHelper;
-}());
-var PolygonHelper = (function () {
-    function PolygonHelper() {
-    }
-    PolygonHelper.draw = function (numberOfSides, width) {
-        push();
-        var angle = TWO_PI / numberOfSides;
-        var radius = width / 2;
-        beginShape();
-        for (var a = 0; a < TWO_PI; a += angle) {
-            var sx = cos(a) * radius;
-            var sy = sin(a) * radius;
-            vertex(sx, sy);
-        }
-        endShape(CLOSE);
-        pop();
-    };
-    return PolygonHelper;
-}());
-var numberOfShapesControl;
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var GameState;
+(function (GameState) {
+    GameState[GameState["MENU"] = 0] = "MENU";
+    GameState[GameState["PLAYING"] = 1] = "PLAYING";
+    GameState[GameState["PAUSED"] = 2] = "PAUSED";
+    GameState[GameState["GAMEOVER"] = 3] = "GAMEOVER";
+})(GameState || (GameState = {}));
+var mapData = {
+    obstacles: [[100, 100]],
+    obstacleSpawnCooldown: 0,
+    gapDistance: 300
+};
+var character = {
+    height: 100.0,
+    acceleration: 1.0,
+    dimensions: [100, 100]
+};
+var score = 0;
+var characterX = 100;
 function setup() {
     console.log("🚀 - Setup initialized - P5 is running");
     createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER).noFill().frameRate(30);
-    numberOfShapesControl = createSlider(1, 30, 15, 1).position(10, 10).style("width", "100px");
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
+    update();
     background(0);
-    translate(width / 2, height / 2);
-    var numberOfShapes = numberOfShapesControl.value();
-    var colours = ColorHelper.getColorsArray(numberOfShapes);
-    var speed = (frameCount / (numberOfShapes * 30)) * 2;
-    for (var i = 0; i < numberOfShapes; i++) {
-        push();
-        var lineWidth = 8;
-        var spin = speed * (numberOfShapes - i);
-        var numberOfSides = 3 + i;
-        var width_1 = 40 * i;
-        strokeWeight(lineWidth);
-        stroke(colours[i]);
-        rotate(spin);
-        PolygonHelper.draw(numberOfSides, width_1);
-        pop();
+    drawCharacter();
+    drawMap();
+    renderScore();
+}
+var update = function () {
+    updateCharacter();
+    updateMap();
+    updateGame();
+};
+var updateCharacter = function () {
+    character.height += character.acceleration;
+    character.acceleration += 0.5;
+    if (character.height > windowHeight) {
+        character.height = windowHeight;
+        character.acceleration = 0;
     }
+};
+var updateMap = function () {
+    if (mapData.obstacles.length < 3 && mapData.obstacleSpawnCooldown == 0) {
+        var gapHeight = Math.floor(Math.random() * (windowHeight * 0.7 - mapData.gapDistance)) + mapData.gapDistance;
+        mapData.obstacles.push([windowWidth, gapHeight]);
+        mapData.obstacleSpawnCooldown = 150;
+    }
+    for (var i = 0; i < mapData.obstacles.length; i++) {
+        mapData.obstacles[i][0] -= 7;
+    }
+    for (var i = 0; i < mapData.obstacles.length; i++) {
+        if (mapData.obstacles[i][0] < -100) {
+            mapData.obstacles.splice(i, 1);
+        }
+    }
+    if (mapData.obstacleSpawnCooldown > 0) {
+        mapData.obstacleSpawnCooldown--;
+    }
+};
+var drawCharacter = function () {
+    ellipse.apply(void 0, __spreadArrays([characterX, character.height], character.dimensions));
+};
+var drawMap = function () {
+    for (var i = 0; i < mapData.obstacles.length; i++) {
+        rect(mapData.obstacles[i][0], 0, 100, mapData.obstacles[i][1]);
+        rect(mapData.obstacles[i][0], mapData.obstacles[i][1] + mapData.gapDistance, 100, windowHeight - mapData.obstacles[i][1] - 200);
+    }
+};
+function keyPressed() {
+    if (keyCode === 32) {
+        character.acceleration = -10;
+    }
+}
+function updateGame() {
+    for (var i = 0; i < mapData.obstacles.length; i++) {
+        if (characterCollides(mapData, character)) {
+            console.log("game over");
+        }
+        if (mapData.obstacles[i][0] < characterX && mapData.obstacles[i][0] > characterX - 7) {
+            score++;
+            console.log(score);
+        }
+    }
+}
+function characterCollides(mapData, character) {
+    return (mapData.obstacles[0][0] < characterX + 50 && mapData.obstacles[0][0] > characterX - 50 && (character.height < mapData.obstacles[0][1]
+        || character.height > mapData.obstacles[0][1] + mapData.gapDistance));
+}
+function renderScore() {
+    textSize(100);
+    fill(255);
+    textStyle(BOLD);
+    text(score, windowWidth / 2 - 25, 100);
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,35 @@
+
+enum GameState {
+  MENU,
+  PLAYING,
+  PAUSED,
+  GAMEOVER
+}
+interface Character {
+  height: number;
+  acceleration: number;
+  dimensions: [number, number];
+}
+interface MapData {
+  obstacles: [[number, number]];
+  obstacleSpawnCooldown: number;
+  gapDistance: number;
+}
+let mapData : MapData = {
+  obstacles: [[100, 100]],
+  obstacleSpawnCooldown: 0,
+  gapDistance: 300
+}
+let character : Character = {
+  height: 100.0,
+  acceleration: 1.0,
+  dimensions: [100, 100]
+}
+
+let score = 0;
+const characterX = 100;
+
+
 function setup() {
   console.log("🚀 - Setup initialized - P5 is running");
   createCanvas(windowWidth, windowHeight)
@@ -8,7 +40,104 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
 function draw() {
+  update();
+  background(0);
+  drawCharacter();
+  drawMap();
+  renderScore();
+}
+
+let update = () => {
+  updateCharacter();
+  updateMap();
+  updateGame();
+
+}
+
+let updateCharacter = () => {
+  //check for spacebar press 
+
+  character.height += character.acceleration;
+  character.acceleration += 0.5;
+  if (character.height > windowHeight) {
+    character.height = windowHeight;
+    character.acceleration = 0;
+  }
+}
+
+let updateMap = () => {
+  //if the mapdata's length is less than 3, add a new obstacle
+  if (mapData.obstacles.length < 3 && mapData.obstacleSpawnCooldown == 0) {
+    //random gap height in the middle 70% of the screen
+
+    let gapHeight = Math.floor(Math.random() * (windowHeight * 0.7 - mapData.gapDistance)) + mapData.gapDistance;
+
+    mapData.obstacles.push([windowWidth, gapHeight]);
+    mapData.obstacleSpawnCooldown = 150;
+  }
+  //for each obstacle, move it to the left
+  //replace this with a for loop
+
+  for (let i = 0; i < mapData.obstacles.length; i++) {
+    mapData.obstacles[i][0] -= 7;
+  }
+
+  //if the first obstacle is off the screen, remove it
+  for (let i = 0; i < mapData.obstacles.length; i++) {
+    if (mapData.obstacles[i][0] < -100) {
+      mapData.obstacles.splice(i, 1);
+    }
+  }
+  //decrement the obstacle spawn cooldown
+  if (mapData.obstacleSpawnCooldown > 0){
+    mapData.obstacleSpawnCooldown--;  
+  }
   
 }
+
+
+const drawCharacter = () => {
+  ellipse(characterX, character.height, ...character.dimensions);
+}
+
+const drawMap = () => {
+  //for each obstacle, draw a rectangle with a gap
+  for (let i = 0; i < mapData.obstacles.length; i++) {
+    rect(mapData.obstacles[i][0], 0, 100, mapData.obstacles[i][1]);
+    rect(mapData.obstacles[i][0], mapData.obstacles[i][1] + mapData.gapDistance, 100, windowHeight - mapData.obstacles[i][1] - 200);
+  } 
+}
+
+function keyPressed () {
+  if (keyCode === 32) {
+    character.acceleration = -10    ;
+  }
+}
+
+function updateGame() {
+  for (let i = 0; i < mapData.obstacles.length; i++){
+    if (characterCollides(mapData, character)) {
+      console.log("game over")
+    }
+    if (mapData.obstacles[i][0] < characterX && mapData.obstacles[i][0] > characterX - 7) {
+      score++;
+      console.log(score);
+    }
+  }
+}
+
+function characterCollides (mapData : MapData, character: Character) {
+  return (mapData.obstacles[0][0] < characterX + 50 && mapData.obstacles[0][0] > characterX - 50 && (character.height < mapData.obstacles[0][1] 
+    || character.height > mapData.obstacles[0][1] + mapData.gapDistance))
+}
+
+function renderScore () {
+  textSize(100);
+  fill(255);
+  //increase font weight
+  textStyle(BOLD);
+  //center the text
+  text(score, windowWidth / 2 - 25, 100);
+  
+} 
